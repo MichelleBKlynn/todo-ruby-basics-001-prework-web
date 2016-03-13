9b4c30d25bbb378569945f497604554cc756b4d5
MichelleBKlynn <michellestaton@Michelles-MBP.home>
2016-03-13 03:38:42 -0400

    Done.

--- spec/ruby_basics_spec.rb
+++ spec/ruby_basics_spec.rb
@@ -1,68 +1,34 @@
-require 'spec_helper'
-require_relative '../lib/ruby_basics.rb'
-
-
-describe "ruby" do
-
-  describe "#division" do
-    it 'given two numbers returns quotient of the first number divided by the second number' do
-      num1 = 42
-      num2 = 7
-      
-      expect(division(num1,num2)).to eq(6)
-    end
-  end
-
-  describe '#assign_variable' do
-    it 'should take an argument of a persons name and assign it to a variable name' do
-      value = "Bob"
-
-      expect(assign_variable(value)).to eq(value)
-    end
-  end
-
-  describe '#argue' do
-    it 'should add an argument to the method argue' do
-      phrase = "I'm right and you are wrong!"
-      
-      expect(argue(phrase)).to eq(phrase)
-    end  
-  end
-
-  describe '#greeting' do
-    it 'should take in two arguments' do
-      greeting = "Hi there, "
-      name = "Bobby!"
-
-      expect{ greeting(greeting, name) }.to_not raise_error
-    end
-
-    it 'should raise an error with one argument' do
-      greeting = "Hi there, "
-
-      expect{ greeting(greeting)}.to raise_error
-    end
-  end
-
-  describe '#return_a_value' do
-    it 'return the phrase "Nice"' do
-      expect(return_a_value).to eq("Nice")
-    end
-  end
-
-  describe '#last_evaluated_value' do
-    it 'return the phrase "expert"' do
-      expect(last_evaluated_value).to eq("expert")
-    end
-  end
-
-  describe '#pizza_party' do
-    it 'has an optional argument that defaults to "cheese" and returns "cheese" when called without an argument' do
-      expect(pizza_party).to eq("cheese")
-    end
-
-    it 'can be called with an argument and return the string that was passed in as an argument' do
-      expect(pizza_party("pepperoni")).to eq("pepperoni")
-    end
-  end
+def division
+   42 / 7
 end
+
+def assign_variable(name = "Bob")
+   "#{name}"
+end
+
+def argue(phrase = "I'm right and you're wrong!")
+   "#{phrase}"
+end
+
+def greeting(greeting, name)
+   "#{greeting} {name}"
+greeting("Hi there," "Bobby!")
+end
+
+def return_a_value(return_a_value = "Nice")
+   "#{return_a_value}"
+end
+
+def last_evaluated_value(last_evaluated_value = "expert")
+  "#{last_evaluated_value}"
+end
+
+def pizza_party(pizza_party = "cheese")
+  "#{pizza_party}"
+end
+
+def pizza_party(pizza_party = "cheese")
+  "#{pizza_party}"
+pizza_party("pepperoni")
+end
+
